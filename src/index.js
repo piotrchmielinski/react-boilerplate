6eadafb7bc9c4df8c029205edad969996d652fd2
Piotr Chmielinski <piotr.chmielinski@gmail.com>
2019-08-30 14:48:15 +0100

    initial upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import style from "./style.scss";
+
+const App = () => {
+  return <div>
+    <h1>React, Babel, Webpack, Sass boilerplate</h1>
+    </div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
